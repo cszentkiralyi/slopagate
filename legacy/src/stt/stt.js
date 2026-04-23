76a377a2b0f2239501d546c2e9d004334fc2a34c
Chris Szentkiralyi <chris.szentkiralyi@gmail.com>
2026-04-23 17:20:17 -0600

    Remove legacy/ from branch, I banish thee to bash heck

--- legacy/src/stt/stt.js
+++
@@ -1,7 +0,0 @@
-//const { replaceTextInFile } = require('./replace.js');
-import { replaceTextInFile } from './replace.js';
-
-// TODO: read ARGV[2] for subtool
-// TODO: based on subtool, call corresponding function with rest of ARGV[] as array arg
-
-replaceTextInFile();
