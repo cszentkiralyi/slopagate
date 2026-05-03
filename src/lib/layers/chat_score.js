6187c671b87753f8e428a021b072e61e46928a2d
Chris Szentkiralyi <chris.szentkiralyi@gmail.com>
2026-05-03 10:11:11 -0600

    More refinements to chat scoring

--- src/lib/layers/chat_score.js
+++ src/lib/layers/chat_score.js
@@ -1,14 +1,15 @@
 const { Logger, lerp, louse } = require('../../util.js');
 
-const T = 5;
+const dT = 3;
 
 const chat_score = ({ messages, config, context_window }) => {
   let max_x = messages.length - 1,
-      turn = config.user_turns || 0;
+      turn = config.user_turns || 0,
       ret = [], i, m;
 
+  const T = turn + dT;
   const f = (x) => louse(x / max_x);
-  const g = (x, t) => (x > 0.5) ? x : Math.max((1 - (t / T)) * x, 0);
+  const g = (x, t) => (x > 0.5) ? f(x) : Math.max((1 - (t / T)) * f(x), 0);
       
   for (i = 0; i <= max_x; i++) {
     m = messages[i];
@@ -16,7 +17,7 @@ const chat_score = ({ messages, config, context_window }) => {
     if (g(f(i), turn) > config.threshold) {
       ret.push(m);
     } else {
-      Logger.log(`chat_score: dropping i=${i} (${(i / max_x).toFixed(3)}) t=${turn} f=${f(x).toFixed(3)} g=${g(f(x), turn).toFixed(3)}`)
+      Logger.log(`chat_score: dropping i=${i} (${(i / max_x).toFixed(3)}) t=${turn} f=${f(i).toFixed(3)} g=${g(f(i), turn).toFixed(3)}`)
     }
   }
 
