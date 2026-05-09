3d64343f4734c64b8087159d630565ee04ad87f3
Chris Szentkiralyi <chris.szentkiralyi@gmail.com>
2026-05-09 07:55:55 -0600

    Activate system_prompt layer, remove dead tool_redundancy layer

--- src/lib/layers/layers.js
+++ src/lib/layers/layers.js
@@ -1,22 +1,16 @@
-//const system_prompt = require('./system_prompt.js');
+const system_prompt = require('./system_prompt.js');
 const tool_age = require('./tool_age.js');
 const tool_length = require('./tool_length.js');
-//const tool_redundancy = require('./tool_redundancy.js');
 const chat_score = require('./chat_score.js');
 const chat_summary = require('./chat_summary.js');
 const tool_error = require('./tool_error.js');
 const model_reasoning = require('./model_reasoning.js');
 const tool_total = require('./tool_total.js');
 
-const placeholder = (arg) => undefined;
-
 module.exports = {
-  //system_prompt,
-  system_prompt:  placeholder,
+  system_prompt,
   tool_age,
   tool_length,
-  //tool_redundancy,
-  tool_redundancy: placeholder,
   chat_score,
   chat_summary,
   tool_error,
